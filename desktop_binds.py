085edc7db233300a673092dfe6d8ae005e799ff7
aniketfuryrocks <prajapati.ani306@gmail.com>
2021-06-17 17:36:03 +0530

    exception rule for Box

--- desktop_binds.py
+++ desktop_binds.py
@@ -12,11 +12,26 @@ CONTAINERS_OUTPUT_DIR = f'{BASE_OUTPUT_DIR}/containers'
 TOP_LEVEL_WIDGETS_OUTPUT_DIR = f'{BASE_OUTPUT_DIR}/top_level_widgets'
 
 # rust keywords which should not match the generated binding's file name
-RUST_KEYWORDS = ['box']
+ALTERNATIVES = {
+    'Box': 'View'
+}
 
 # list of all top level widgets
 TOP_LEVEL_WIDGET_LIST = [
-    'Window'
+    'Window',
+]
+
+BLACKLIST = [
+    'ModelButton',
+    'ToolButton',
+    'PopoverMenu',
+    'Container',
+    'ShortcutsSection',
+    'ShortcutsWindow',
+    'ShortcutLabel',
+    'ShortcutsShortcut',
+    'StackSidebar',
+    'GLArea',
 ]
 
 # gxi-rs docs.rs url
@@ -24,7 +39,7 @@ BASE_URL = 'https://docs.rs/gtk/0.9.2/gtk'
 
 
 # converts string to snake case and if the name is a rust keyword then appends a _ to it
-def to_rust_valid(value):
+def to_snake_case(value):
     res = [value[0].lower()]
     for c in value[1:]:
         if c in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
@@ -32,18 +47,18 @@ def to_rust_valid(value):
             res.append(c.lower())
         else:
             res.append(c)
-    result = ''.join(res)
-    if RUST_KEYWORDS.__contains__(result):
-        result += "_"
-    return result
+    return ''.join(res)
 
 
-async def scrap(tr, container_mod_file, widgets_mod_file):
+async def scrap(tr, top_level_mod_file, container_mod_file, widgets_mod_file):
     # get the name of the widget
     a = tr.select_one("a")
-    gtk_node = a['title'].split(' ')[0].split('::')[1]
+    struct = a['title'].split(' ')[0].split('::')[1]
+
+    if BLACKLIST.__contains__(struct):
+        return
     # if it ends with Class, Group or Builder then it's not a widget
-    if not gtk_node.endswith('Class') and not gtk_node.endswith('Group') and not gtk_node.endswith(
+    if not struct.endswith('Class') and not struct.endswith('Group') and not struct.endswith(
             'Builder'):
         # url to doc of the widget
         gtk_node_url = f"{BASE_URL}/{a['href']}"
@@ -55,36 +70,55 @@ async def scrap(tr, container_mod_file, widgets_mod_file):
         # if it not a container then check if it is a widget
         if not is_a_container:
             is_a_widget = req.text.__contains__('impl-IsA%3CWidget%3E')
-        # generate valid name for the node
-        gtk_node_file_name = to_rust_valid(gtk_node)
-        # entry in mod.rs
-        mod = f"pub mod {gtk_node_file_name};\npub use {gtk_node_file_name}::*;\n"
 
         if is_a_widget:
+            # generate valid name for the node
+            node_name = struct
+            file_name = ''
+            # check if there is any alternative
+            if ALTERNATIVES.__contains__(struct):
+                file_name = to_snake_case(ALTERNATIVES[struct])
+                node_name = ALTERNATIVES[struct]
+            else:
+                file_name = to_snake_case(struct)
+            # entry in mod.rs
+            mod = f"mod {file_name};\npub use {file_name}::*;\n"
+
             print(f"{BASE_URL}/{a['href']}")
-            print(f"{gtk_node} is_a_container: {is_a_container} is_a_widget: {is_a_widget}")
+            print(
+                f"{struct} is_a_container: {is_a_container} is_a_widget: {is_a_widget}")
             # doc
-            doc_comment = f"//! [{gtk_node}]({gtk_node_url})"
+            doc_comment = f"//! [{struct}]({gtk_node_url})"
             # write to file accordingly
             if is_a_container:
-                output_file = open(f"{CONTAINERS_OUTPUT_DIR}/{gtk_node_file_name}.rs", 'w')
-                output_file.write(f"""{doc_comment}
+                if TOP_LEVEL_WIDGET_LIST.__contains__(struct):
+                    output_file = open(
+                        f"{TOP_LEVEL_WIDGETS_OUTPUT_DIR}/{node_name}.rs", 'w')
+                    output_file.write(f"""{doc_comment}
 use crate::*;
-create_desktop_container!({gtk_node});
-impl_desktop_container!({gtk_node});""")
-                container_mod_file.write(mod)
+create_desktop_container!({node_name} {struct});
+impl_desktop_container!(TopLevelWidget {node_name} {struct});""")
+                    top_level_mod_file.write(mod)
+                else:
+                    output_file = open(
+                        f"{CONTAINERS_OUTPUT_DIR}/{node_name}.rs", 'w')
+                    output_file.write(f"""{doc_comment}
+use crate::*;
+create_desktop_container!({node_name} {struct});
+impl_desktop_container!(ContainerWidget {node_name} {struct});""")
+                    container_mod_file.write(mod)
             else:
-                output_file = open(f"{WIDGETS_OUTPUT_DIR}/{gtk_node_file_name}.rs", 'w')
+                output_file = open(f"{WIDGETS_OUTPUT_DIR}/{node_name}.rs", 'w')
                 output_file.write(f"""{doc_comment}
 use crate::*;
-create_widget!({gtk_node});
-impl_widget!({gtk_node});""")
+create_widget!({node_name} {struct});
+impl_widget!({node_name} {struct});""")
                 widgets_mod_file.write(mod)
 
 
 async def main():
     # delete old binds
-    for path in [BASE_OUTPUT_DIR, CONTAINERS_OUTPUT_DIR, WIDGETS_OUTPUT_DIR]:
+    for path in [BASE_OUTPUT_DIR, TOP_LEVEL_WIDGETS_OUTPUT_DIR, CONTAINERS_OUTPUT_DIR, WIDGETS_OUTPUT_DIR]:
         if os.path.isdir(path):
             shutil.rmtree(path)
         os.mkdir(path)
@@ -94,10 +128,12 @@ async def main():
     prev_h2 = html.select_one("#structs")
     table = prev_h2.find_next_sibling('table')
     # mod files
+    top_level_mod_file = open(f'{TOP_LEVEL_WIDGETS_OUTPUT_DIR}/mod.rs', 'w')
     container_mod_file = open(f'{CONTAINERS_OUTPUT_DIR}/mod.rs', 'w')
     widgets_mod_file = open(f'{WIDGETS_OUTPUT_DIR}/mod.rs', 'w')
     # generate bindings for all structs
-    tasks = [scrap(tr, container_mod_file, widgets_mod_file) for tr in table.children]
+    tasks = [scrap(tr, top_level_mod_file, container_mod_file,
+                   widgets_mod_file) for tr in table.children]
     await asyncio.wait(tasks)
 
 
